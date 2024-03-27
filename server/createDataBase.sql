be88e29177361988ba4e4faae4013a94889d796a
hadas heller <handesaim3a9@mbjcomp.org.il>
2024-03-28 01:25:41 +0200

    sql

--- server/createDataBase.sql
+++ server/createDataBase.sql
@@ -4,51 +4,58 @@ CREATE DATABASE IF NOT EXISTS hospital_management;
 -- Use the hospital_management database
 USE hospital_management;
 
--- Create PersonalData table
-CREATE TABLE IF NOT EXISTS PersonalData (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    city VARCHAR(100),
-    street VARCHAR(100),
-    street_number VARCHAR(10),
-    birth_date DATE,
-    phone VARCHAR(20),
-    mobile_phone VARCHAR(20)
-);
+-- Create schema if not exists
+CREATE SCHEMA IF NOT EXISTS hospital_management;
 
--- Create CoronaData table
-CREATE TABLE IF NOT EXISTS CoronaData (
-    member_id INT  PRIMARY KEY,
-    positive_test_date DATE,
-    recovery_date DATE,
-    FOREIGN KEY (member_id) REFERENCES PersonalData(id)
-);
+-- -----------------------------------------------------
+-- Table `personaldata`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `personaldata`;
 
--- Create Vaccinations table
-CREATE TABLE IF NOT EXISTS Vaccinations (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    member_id INT,
-    vaccination_date DATE,
-    vaccination_type VARCHAR(50),
-    FOREIGN KEY (member_id) REFERENCES PersonalData(id)
-);
+CREATE TABLE IF NOT EXISTS `personaldata` (
+  `id` INT NOT NULL,
+  `first_name` VARCHAR(50) NULL DEFAULT NULL,
+  `last_name` VARCHAR(50) NULL DEFAULT NULL,
+  `city` VARCHAR(100) NULL DEFAULT NULL,
+  `street` VARCHAR(100) NULL DEFAULT NULL,
+  `street_number` VARCHAR(10) NULL DEFAULT NULL,
+  `birth_date` DATE NULL DEFAULT NULL,
+  `phone` VARCHAR(20) NULL DEFAULT NULL,
+  `mobile_phone` VARCHAR(20) NULL DEFAULT NULL,
+  `image_data` LONGBLOB NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-/*this is not completely necessary*/
-DELIMITER //
-CREATE TRIGGER before_vaccination_insert
-BEFORE INSERT ON vaccination
-FOR EACH ROW
-BEGIN
-    DECLARE member_vaccination_count INT;
-    
-    SELECT COUNT(*) INTO member_vaccination_count
-    FROM vaccination
-    WHERE member_id = NEW.member_id;
+-- -----------------------------------------------------
+-- Table `coronadata`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `coronadata`;
 
-    IF member_vaccination_count >= 4 THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Cannot insert more than four vaccinations for a member';
-    END IF;
-END //
-DELIMITER ;
+CREATE TABLE IF NOT EXISTS `coronadata` (
+  `member_id` INT NOT NULL,
+  `positive_test_date` DATE NULL DEFAULT NULL,
+  `recovery_date` DATE NULL DEFAULT NULL,
+  PRIMARY KEY (`member_id`),
+  CONSTRAINT `coronadata_ibfk_1`
+    FOREIGN KEY (`member_id`)
+    REFERENCES `personaldata` (`id`)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+-- -----------------------------------------------------
+-- Table `vaccinations`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `vaccinations`;
+
+CREATE TABLE IF NOT EXISTS `vaccinations` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `member_id` INT NULL DEFAULT NULL,
+  `vaccination_date` DATE NULL DEFAULT NULL,
+  `vaccination_type` VARCHAR(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `member_id` (`member_id` ASC) VISIBL
