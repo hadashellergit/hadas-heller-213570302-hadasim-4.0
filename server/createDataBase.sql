8dfe4495d780d977ed25e3080b92d502c4b7a5df
hadas heller <handesaim3a9@mbjcomp.org.il>
2024-03-28 01:27:36 +0200

    sqsq

--- server/createDataBase.sql
+++ server/createDataBase.sql
@@ -58,4 +58,35 @@ CREATE TABLE IF NOT EXISTS `vaccinations` (
   `vaccination_date` DATE NULL DEFAULT NULL,
   `vaccination_type` VARCHAR(50) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `member_id` (`member_id` ASC) VISIBL
+  INDEX `member_id` (`member_id` ASC) VISIBLE,
+  CONSTRAINT `vaccinations_ibfk_1`
+    FOREIGN KEY (`member_id`)
+    REFERENCES `personaldata` (`id`)
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 29
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS `before_vaccination_insert` $$
+CREATE
+DEFINER=`root`@`localhost`
+TRIGGER `hospital_management`.`before_vaccination_insert`
+BEFORE INSERT ON `hospital_management`.`vaccinations`
+FOR EACH ROW
+BEGIN
+    DECLARE member_vaccination_count INT;
+    
+    -- Get the count of existing vaccination records for the member
+    SELECT COUNT(*) INTO member_vaccination_count
+    FROM hospital_management.vaccinations
+    WHERE member_id = NEW.member_id;
+    
+    -- Raise an error if the count exceeds four
+    IF member_vaccination_count >= 4 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Cannot insert more than four vaccinations for a member';
+    END IF;
+END$$
