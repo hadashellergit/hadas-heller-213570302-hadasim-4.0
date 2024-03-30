d1a96c9d0c8301c794215f82eb41e2a2a48a8498
hadashellergit <164341547+hadashellergit@users.noreply.github.com>
2024-03-30 21:04:46 +0300

    Update vaccinationModel.js

--- server/models/vaccinationModel.js
+++ server/models/vaccinationModel.js
@@ -1,20 +1,34 @@
 const db = require('../config/db');
-const fs = require('fs'); 
 
-//GET
-const find =async ()=>{
+// Function to check if previous vaccinations are late
+const checkVaccinationDates = async (memberId, givenDate) => {
+  //chacks the number of later vaccination dates in the table to inform the user
   try {
-     //fetch every row from vaccination table
-    const vaccintations = await db.query(`SELECT * FROM vaccinations`);
-    return members;
+    const query = `
+      SELECT COUNT(*) AS invalid_count
+      FROM vaccinations
+      WHERE member_id = ? AND vaccination_date > ?
+    `;
+    const [result] = await db.query(query, [memberId, givenDate]);
+    const invalidCount = result.invalid_count;
+
+    return invalidCount > 0;
   } catch (error) {
     throw error;
   }
-}
+};
 
-const findUnVaccinatedCount = async (req,res) => {
+const find = async () => {
   try {
-    //query counts the number of members in the personaldata table who do not have corresponding entries in the vaccinations table 
+    const vaccinations = await db.query(`SELECT * FROM vaccinations`);
+    return vaccinations;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const findUnVaccinatedCount = async () => {
+  try {
     const query = `
       SELECT COUNT(m.id) AS unvaccinated_count
       FROM personaldata m
@@ -22,7 +36,6 @@ const findUnVaccinatedCount = async (req,res) => {
       WHERE v.member_id IS NULL
     `;
     const result = await db.query(query);
-    //result is an array.. so..
     const unvaccinatedCount = result[0].unvaccinated_count;
     return unvaccinatedCount;
   } catch (error) {
@@ -30,29 +43,26 @@ const findUnVaccinatedCount = async (req,res) => {
   }
 };
 
-//POST
 const create = async (vaccinData) => {
   try {
-    // query to check if the member has less than four vaccinations (there is a sql trigger with the same constraint)
     const [vaccinationCountRows] = await db.query('SELECT COUNT(*) AS vaccination_count FROM vaccinations WHERE member_id = ?', [vaccinData.member_id]);
     const vaccinationCount = vaccinationCountRows.vaccination_count;
 
-    //if the member has more then four vaccination info
     if (vaccinationCount >= 4) {
       throw new Error('Cannot insert more than four vaccinations for a member');
     }
 
-    // else Insert the vaccination data
     await db.query('INSERT INTO vaccinations (member_id, vaccination_date, vaccination_type) VALUES (?, ?, ?)', [vaccinData.member_id, vaccinData.vaccination_date, vaccinData.vaccination_type]);
+    
     return { message: 'vaccination created successfully' };
   } catch (error) {
     throw error;
   }
 };
 
-
 module.exports = {
   find,
   findUnVaccinatedCount,
-  create
+  create,
+  checkVaccinationDates // Add the checkVaccinationDates function to module exports
 };
